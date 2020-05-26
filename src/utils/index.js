f8f3d7d13e0a4813eb2dab334e218600e1883ba8
Flavio <flavio.farias@ccc.ufcg.edu.br>
2020-05-26 04:47:50 -0300

    Registrar categoria adicionado; upload de imagems para Storage do Firebase adicionado;

---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+export function randomImageName(length, ext) {
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let string = '';
+  const max = characters.length - 1;
+  const min = 0;
+  for (let i = 0; i < length; i += 1) {
+    string += characters[Math.floor(Math.random() * (+max - +min) + +min)];
+  }
+  return string + ext;
+}
