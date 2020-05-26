85f1bfa342deca8a2dfe2913e454cb3020ded8be
Flavio <flavio.farias@ccc.ufcg.edu.br>
2020-05-26 20:18:24 -0300

    Auth Flow adicionado a StackScreen

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import '../services/api';
+import { auth } from 'firebase';
 
-import Home from '../screens/Home';
+import Category from '../screens/Category';
+import Login from '../screens/Login';
+import Loading from '../screens/Loading';
+import Profile from '../screens/Profile';
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  auth().onAuthStateChanged((user) => setLoggedIn(!!user));
   return (
 
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        {(!loggedIn
+          ? (
+            <>
+              <Stack.Screen name="Loading" component={Loading} options={{ headerShown: false }} />
+              <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="Category" component={Category} options={{ headerShown: false }} />
+              <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+            </>
+          )
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Loading.js
+++ src/screens/Loading.js
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { auth } from 'firebase';
 import { Container, Text } from '../components';
+import '../services/api';
+
 
 export default function Loading() {
   const navigation = useNavigation();
 
   function checkIfLoggedIn() {
     auth().onAuthStateChanged(
-      (user) => navigation.navigate(user ? 'Home' : 'Login'),
+      (user) => {
+        if (!user) navigation.navigate('Login');
+      },
     );
   }
 
-  useEffect(() => checkIfLoggedIn());
+  useFocusEffect(() => checkIfLoggedIn(), []);
 
   return (
     <Container middle center>
