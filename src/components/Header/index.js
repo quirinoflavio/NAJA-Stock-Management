a74ebf9a1505401f6e67cf016fbbecd9d59383ec
Flavio <flavio.farias@ccc.ufcg.edu.br>
2020-05-26 18:19:31 -0300

    Perfil adicionado; Header link Perfil

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import { StyleSheet, Image, AsyncStorage } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
+import Constants from 'expo-constants';
+import { useNavigation } from '@react-navigation/native';
+import { currentUser } from '../../services/auth';
 import { Container, Text, Button } from '../Base';
 import { theme } from '../../constants';
 
-const getProfile = async () => {
-  let profile;
-  try {
-    profile = await AsyncStorage.getItem('profile') || 'none';
-  } catch (error) {
-    console.log(error.message);
-  } return profile;
-};
+function getProfile() {
+  return currentUser();
+}
+console.log(Constants.statusBarHeight);
 
 export default function Header() {
-  const profile = getProfile;
-
+  const profile = getProfile();
+  const navitation = useNavigation();
   return (
     <Container flex={false} row center space="between" style={styles.header}>
-      <Text h1 bold>
-        Home Page
+      <Text h2 bold>
+        Categorias
       </Text>
-      <Button>
+      <Button style={styles.button} onPress={() => navitation.navigate('Profile')}>
         <Image
           size={theme.sizes.base * 2.7}
           style={styles.avatar}
-          source={profile.avatar}
+          source={{ uri: profile.photoURL }}
         />
       </Button>
     </Container>
@@ -33,10 +32,17 @@ export default function Header() {
 
 const styles = StyleSheet.create({
   header: {
+    maxHeight: 60,
+    backgroundColor: theme.colors.primary,
     paddingHorizontal: theme.sizes.base * 2,
+    marginTop: Constants.statusBarHeight,
   },
   avatar: {
     height: theme.sizes.base * 2.2,
     width: theme.sizes.base * 2.2,
+    borderRadius: 20,
+  },
+  button: {
+    backgroundColor: 'transparent',
   },
 });
